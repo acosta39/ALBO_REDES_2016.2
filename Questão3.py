649324967fadbeae8af36f3e543b5b06e773e9c1
acosta39 <arnaldocosta.filho@gmail.com>
2016-08-17 20:56:30 -0300

    Add files via upload

---
+++ Questão3.py
@@ -0,0 +1,16 @@
+print ('CALCULOS')
+
+soma = 200 + 250
+print('a) 200 + 250 =',soma)
+
+divisao = 200 / 8
+print('b) 200 / 8 =',divisao)
+
+multiplicacao = 200 * 7
+print('c) 200 * 7 =', multiplicacao)
+
+subtracao = 800 - 10
+print('d) 800 - 10 =', subtracao)
+
+potencia = 2 **10
+print('e) 2 elevado a décima potência =',potencia)
