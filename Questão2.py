f226673f6b812874e7784f450558545cc155cc93
acosta39 <arnaldocosta.filho@gmail.com>
2016-08-17 20:56:02 -0300

    Add files via upload

---
+++ Questão2.py
@@ -0,0 +1,11 @@
+texto = ('"Eu tenho um sonho. O sonho de ver meus filhos julgados por sua personalidade, não pela cor de sua pele."')
+novotexto = texto.upper()
+print (novotexto)
+
+texto2 = ('"Eu tenho um sonho. O sonho de ver meus filhos julgados por sua personalidade, não pela cor de sua pele."')
+novotexto2 = texto2.lower()
+print (novotexto2)
+
+texto3 = ('"Eu tenho um sonho. O sonho de ver meus filhos julgados por sua personalidade, não pela cor de sua pele."')
+novotexto3 = texto3.title()
+print (novotexto3)
